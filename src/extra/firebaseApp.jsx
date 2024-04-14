e98a8b2c297756c14524117ef8725110b6a05f5b
aynry <90700601+aynry@users.noreply.github.com>
2024-04-14 12:21:31 +1000

    Update firebaseApp.jsx

--- src/extra/firebaseApp.jsx
+++ src/extra/firebaseApp.jsx
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyAchFiwigFdvybtDzJeA1e5qfVf8jthFSk",
+  apiKey: "own API",
   authDomain: "moneymate-44dea.firebaseapp.com",
   projectId: "moneymate-44dea",
   storageBucket: "moneymate-44dea.appspot.com",
   messagingSenderId: "314604223200",
-  appId: "1:314604223200:web:20ca72af77f643685d6e85"
+  appId: "Own ID"
 };
 
 // Initialize Firebase
