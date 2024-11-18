7d4b241cd9b0e48956f20bd13527f41a5684c8c5
aynry <90700601+aynry@users.noreply.github.com>
2024-11-18 12:32:23 +1100

    Update firebaseApp.jsx

--- src/extra/firebaseApp.jsx
+++ src/extra/firebaseApp.jsx
@@ -1,13 +1,21 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "own API",
-  authDomain: "moneymate-44dea.firebaseapp.com",
-  projectId: "moneymate-44dea",
-  storageBucket: "moneymate-44dea.appspot.com",
-  messagingSenderId: "314604223200",
-  appId: "Own ID"
+  apiKey: "key",
+  authDomain: "moneymate-8e5f7.firebaseapp.com",
+  projectId: "moneymate-8e5f7",
+  storageBucket: "moneymate-8e5f7.firebasestorage.app",
+  messagingSenderId: "130358027299",
+  appId: "ID",
+  measurementId: ""
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 export default firebaseApp;
