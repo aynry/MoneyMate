a0d4d732692a2608980ae03ab6d9632e4978bef4
aynry <90700601+aynry@users.noreply.github.com>
2024-11-18 12:59:25 +1100

    Update firebaseApp.jsx

--- src/extra/firebaseApp.jsx
+++ src/extra/firebaseApp.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyB1ZFkRp3fW-PrAPuOqOPtVfmJ2ASePxRU",
+  authDomain: "moneymate-8e5f7.firebaseapp.com",
+  projectId: "moneymate-8e5f7",
+  storageBucket: "moneymate-8e5f7.firebasestorage.app",
+  messagingSenderId: "130358027299",
+  appId: "1:130358027299:web:cc919809a672c565467d96",
+  measurementId: "G-9NPCRLK6ML"
 };
 
 // Initialize Firebase
